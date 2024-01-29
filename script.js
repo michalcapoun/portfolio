2f23e7b23c7dcc83fb67e7faa71cc13df600dfc6
michalcapoun <michal.capoun@gmail.com>
2024-01-29 15:51:34 +0100

    navbar remake

--- script.js
+++ script.js
@@ -1,67 +1,78 @@
-// HAMBURGER MENU
+// NAVBAR MENU TOGGLE
 
 document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.querySelector(".hamburger-icon");
-  const menuLinks = document.querySelector(".hamburger-links");
+  const navMenu = document.querySelector("#nav-menu");
+  const navToggle = document.querySelector("#nav-toggle");
+  const navClose = document.querySelector("#nav-close");
 
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("open");
-    menuLinks.classList.toggle("open");
-  });
-
-  document.querySelectorAll(".hamburger-link").forEach((link) => {
-    link.addEventListener("click", () => {
-      hamburger.classList.remove("open");
-      menuLinks.classList.remove("open");
+  if (navToggle) {
+    navToggle.addEventListener("click", () => {
+      navMenu.classList.add("show-menu");
     });
-  });
+  }
+
+  if (navClose) {
+    navClose.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLink = document.querySelectorAll(".nav-link");
+
+  const linkAction = () => {
+    const navMenu = document.querySelector("#nav-menu");
+    navMenu.classList.remove("show-menu");
+  };
+  navLink.forEach((n) => n.addEventListener("click", linkAction));
 });
 
 // NAVBAR SCROLL EFFECT
 
-const navbarLinks = document.querySelectorAll(".desktop-link");
-let currentActiveIndex = -1;
+// const navbarLinks = document.querySelectorAll(".desktop-link");
+// let currentActiveIndex = -1;
 
-const updateActiveNavbarLink = () => {
-  const scrollOffset = 200;
-  const currentPosition = window.scrollY + scrollOffset;
+// const updateActiveNavbarLink = () => {
+//   const scrollOffset = 200;
+//   const currentPosition = window.scrollY + scrollOffset;
 
-  navbarLinks.forEach((link, index) => {
-    const targetSection = document.querySelector(link.hash);
-    if (!link.hash || !targetSection) return;
+//   navbarLinks.forEach((link, index) => {
+//     const targetSection = document.querySelector(link.hash);
+//     if (!link.hash || !targetSection) return;
 
-    const isInSectionView =
-      currentPosition >= targetSection.offsetTop &&
-      currentPosition <= targetSection.offsetTop + targetSection.offsetHeight;
+//     const isInSectionView =
+//       currentPosition >= targetSection.offsetTop &&
+//       currentPosition <= targetSection.offsetTop + targetSection.offsetHeight;
 
-    if (isInSectionView && index !== currentActiveIndex) {
-      navbarLinks.forEach((el) => el.classList.remove("active"));
-      link.classList.add("active");
-      currentActiveIndex = index;
-    } else if (!isInSectionView && index === currentActiveIndex) {
-      link.classList.remove("active");
-      currentActiveIndex = -1;
-    }
-  });
-};
+//     if (isInSectionView && index !== currentActiveIndex) {
+//       navbarLinks.forEach((el) => el.classList.remove("active"));
+//       link.classList.add("active");
+//       currentActiveIndex = index;
+//     } else if (!isInSectionView && index === currentActiveIndex) {
+//       link.classList.remove("active");
+//       currentActiveIndex = -1;
+//     }
+//   });
+// };
 
-window.addEventListener("load", updateActiveNavbarLink);
-window.addEventListener("scroll", updateActiveNavbarLink);
+// window.addEventListener("load", updateActiveNavbarLink);
+// window.addEventListener("scroll", updateActiveNavbarLink);
 
 // COPY PHONE NUMBER TO CLIPBOARD
 
-document.addEventListener("DOMContentLoaded", () => {
-  const phoneNumberElement = document.getElementById("phoneNumber");
-  const copyMessageElement = document.getElementById("copyMessage");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const phoneNumberElement = document.getElementById("phoneNumber");
+//   const copyMessageElement = document.getElementById("copyMessage");
 
-  const copyPhoneNumber = () => {
-    navigator.clipboard.writeText(phoneNumberElement.innerText).then(() => {
-      copyMessageElement.style.display = "block";
-      setTimeout(() => {
-        copyMessageElement.style.display = "none";
-      }, 3000);
-    });
-  };
+//   const copyPhoneNumber = () => {
+//     navigator.clipboard.writeText(phoneNumberElement.innerText).then(() => {
+//       copyMessageElement.style.display = "block";
+//       setTimeout(() => {
+//         copyMessageElement.style.display = "none";
+//       }, 3000);
+//     });
+//   };
 
-  phoneNumberElement.addEventListener("click", copyPhoneNumber);
-});
+//   phoneNumberElement.addEventListener("click", copyPhoneNumber);
+// });
