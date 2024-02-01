f3cf659501f2b01a89f7868f4b170fadf3b1898b
michalcapoun <michal.capoun@gmail.com>
2024-02-01 12:08:14 +0100

    navbar for mobile

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
 // NAVBAR MENU TOGGLE
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const navMenu = document.querySelector("#nav-menu");
-//   const navToggle = document.querySelector("#nav-toggle");
-//   const navClose = document.querySelector("#nav-close");
+document.addEventListener("DOMContentLoaded", () => {
+  const navMenu = document.querySelector("#nav-menu");
+  const navToggle = document.querySelector("#nav-toggle");
+  const navClose = document.querySelector("#nav-close");
 
-//   if (navToggle) {
-//     navToggle.addEventListener("click", () => {
-//       navMenu.classList.add("show-menu");
-//     });
-//   }
+  if (navToggle) {
+    navToggle.addEventListener("click", () => {
+      navMenu.classList.add("show-menu");
+    });
+  }
 
-//   if (navClose) {
-//     navClose.addEventListener("click", () => {
-//       navMenu.classList.remove("show-menu");
-//     });
-//   }
-// });
+  if (navClose) {
+    navClose.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+    });
+  }
+});
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const navLink = document.querySelectorAll(".nav-link");
+document.addEventListener("DOMContentLoaded", () => {
+  const navLink = document.querySelectorAll(".nav-link");
 
-//   const linkAction = () => {
-//     const navMenu = document.querySelector("#nav-menu");
-//     navMenu.classList.remove("show-menu");
-//   };
-//   navLink.forEach((n) => n.addEventListener("click", linkAction));
-// });
+  const linkAction = () => {
+    const navMenu = document.querySelector("#nav-menu");
+    navMenu.classList.remove("show-menu");
+  };
+  navLink.forEach((n) => n.addEventListener("click", linkAction));
+});
 
 // NAVBAR ACTIVE SECTION
 
-const navbarLinks = document.querySelectorAll(".nav-link");
-let currentActiveIndex = -1;
+// const navbarLinks = document.querySelectorAll(".nav-link");
+// let currentActiveIndex = -1;
 
-const updateActiveNavbarLink = () => {
-  const scrollOffset = 200;
-  const currentPosition = window.scrollY + scrollOffset;
+// const updateActiveNavbarLink = () => {
+//   const scrollOffset = 200;
+//   const currentPosition = window.scrollY + scrollOffset;
 
-  navbarLinks.forEach((link, index) => {
-    const targetSection = document.querySelector(link.hash);
-    if (!link.hash || !targetSection) return;
+//   navbarLinks.forEach((link, index) => {
+//     const targetSection = document.querySelector(link.hash);
+//     if (!link.hash || !targetSection) return;
 
-    const isInSectionView =
-      currentPosition >= targetSection.offsetTop &&
-      currentPosition <= targetSection.offsetTop + targetSection.offsetHeight;
+//     const isInSectionView =
+//       currentPosition >= targetSection.offsetTop &&
+//       currentPosition <= targetSection.offsetTop + targetSection.offsetHeight;
 
-    if (isInSectionView && index !== currentActiveIndex) {
-      navbarLinks.forEach((el) => el.classList.remove("active"));
-      link.classList.add("active");
-      currentActiveIndex = index;
-    } else if (!isInSectionView && index === currentActiveIndex) {
-      link.classList.remove("active");
-      currentActiveIndex = -1;
-    }
-  });
-};
+//     if (isInSectionView && index !== currentActiveIndex) {
+//       navbarLinks.forEach((el) => el.classList.remove("active"));
+//       link.classList.add("active");
+//       currentActiveIndex = index;
+//     } else if (!isInSectionView && index === currentActiveIndex) {
+//       link.classList.remove("active");
+//       currentActiveIndex = -1;
+//     }
+//   });
+// };
 
-window.addEventListener("load", updateActiveNavbarLink);
-window.addEventListener("scroll", updateActiveNavbarLink);
+// window.addEventListener("load", updateActiveNavbarLink);
+// window.addEventListener("scroll", updateActiveNavbarLink);
